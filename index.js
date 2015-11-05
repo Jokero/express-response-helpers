1e1f2546b643501150cc0845c4126b55416b33b4
DmitryKirilyuk <gk.joker@gmail.com>
2015-11-05 15:27:24 +0300

    extracted transformRequest from setParameters and removed setParameters

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+exports.transformRequest  = require('./lib/transformRequest');
 exports.transformResponse = require('./lib/transformResponse');
 exports.transformer       = require('transformer');
 exports.HttpError         = require('http-error');
@@ -5,7 +6,6 @@ exports.HttpError         = require('http-error');
 exports.middleware  = {
     addToResponseSendDataMethod:   require('./lib/middleware/addToResponseSendDataMethod'),
     addToResponseSendObjectMethod: require('./lib/middleware/addToResponseSendObjectMethod'),
-    setParameters:                 require('./lib/middleware/setParameters'),
     fetchObject:                   require('./lib/middleware/fetchObject'),
     setFormat:                     require('./lib/middleware/setFormat')
 };
