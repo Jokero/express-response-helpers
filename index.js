ed8e48b3e411f288c2f17014c1b3b88d92cd722d
Dmitry Kirilyuk <gk.joker@gmail.com>
2015-12-11 12:07:32 +0300

    moved setParameters to separate repo

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-exports.transformRequest  = require('./lib/transformRequest');
 exports.transformResponse = require('./lib/transformResponse');
-exports.transformer       = require('transformer');
 
 exports.middleware  = {
     addToResponseSendDataMethod:   require('./lib/middleware/addToResponseSendDataMethod'),
-    addToResponseSendObjectMethod: require('./lib/middleware/addToResponseSendObjectMethod'),
-    setParameters:                 require('./lib/middleware/setParameters')
+    addToResponseSendObjectMethod: require('./lib/middleware/addToResponseSendObjectMethod')
 };
