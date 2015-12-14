e5d2cf0a9d072f5985c7f27ab392d8309a78af22
Dmitry Kirilyuk <gk.joker@gmail.com>
2015-12-14 19:31:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 exports.transformResponse = require('./lib/transformResponse');
 
-exports.middleware  = {
+exports.middleware = {
     addSendDataMethod:   require('./lib/middleware/addSendDataMethod'),
     addSendObjectMethod: require('./lib/middleware/addSendObjectMethod')
 };
